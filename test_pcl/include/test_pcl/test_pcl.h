6241a91bf665286e2e3087e32ed31940b923fb6d
Manuel Bonilla <josemanuelbonilla@gmail.com>
2017-11-04 18:32:44 +0100

    First commit

---
+++ test_pcl/include/test_pcl/test_pcl.h
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+// PCL specific includes
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl_ros/point_cloud.h>
+
+#include <pcl/ModelCoefficients.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/surface/concave_hull.h>
